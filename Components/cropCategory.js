370e2625fc7ca016546fa4681670bf55d10df905
prashantsonarwadikar@gmail.com <prashantitsgithub>
2025-04-17 23:36:51 +0530

    Remove existing files

--- Components/cropCategory.js
+++
@@ -1,9 +0,0 @@
-const CropCategory = () => {
-    return (
-        <section className="future-farming">
-            
-        </section>
-    );
-};
-
-export default CropCategory;
--- pages/CropDetail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CropDetail = () => {
-  return (
-    <div>CropDetail</div>
-  )
-}
-
-export default CropDetail;
